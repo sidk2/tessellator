11732ba16b90bc14507989b84b74b3c67614537b
sid <skninja6@gmail.com>
2020-06-02 22:16:24 -0700

    math and prints

--- tesselator/src/tesselator/Main.java
+++ tesselator/src/tesselator/Main.java
@@ -1,10 +1,16 @@
 package tesselator;
 
+import java.util.ArrayList;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		Parser p = new Parser("Appa.obj");
-		p.parseVertices();
+		ArrayList<Shape> s = p.getFaces();
+		for(Shape sh:s)
+		{
+			System.out.print((new ShapeData(sh)).toString());
+		}
 	}
 
 }
--- tesselator/src/tesselator/ShapeData.java
+++ tesselator/src/tesselator/ShapeData.java
@@ -8,4 +8,22 @@ public class ShapeData {
 		sideLengths = s.getSideLengths();
 		angles = s.getAngles();
 	}
+	public String toString()
+	{
+		String s = "";
+		s+= "side lengths: ";
+		for(double d:sideLengths)
+		{
+			s+=d;
+			s+=", ";
+		}
+		s+="|angles: ";
+		for(double d:angles)
+		{
+			s+=d;
+			s+=", ";
+		}
+		s+="\n";
+		return s;
+	}
 }
