5e86afdcb9bc87b0f7bcd53b1bf67ce41879f176
rohildshah <shah5963@gmail.com>
2020-06-03 00:06:25 -0700

    visualizer sides method proto

--- tesselator/src/tesselator/Visualizer.java
+++ tesselator/src/tesselator/Visualizer.java
@@ -1,5 +1,51 @@
 package tesselator;
 
+import java.util.*;
+
+
 public class Visualizer {
+    ArrayList<Shape> shapes;
+
+    public void setShapes(ArrayList<Shape> shapes) {
+        this.shapes = shapes;
+    }
+
+    public ArrayList<Shape> getShapes() {
+        return shapes;
+    }
+
+    public ArrayList<Double> toDoublearray(double[] arr) {
+        ArrayList<Double> result = new ArrayList<Double>();
+        for (double dub : arr) {
+            result.add(new Double(dub));
+        }
+        return result;
+    }
+
+    public ArrayList<Shape> findSides(Shape shape) {
+        ArrayList<ArrayList<Double>> vertices = shape.getVertices();
+        ArrayList<Shape> result = new ArrayList<Shape>();
+
+        for (ArrayList<Double> vertex : vertices) {
+            ArrayList<Double> sideLengths = toDoublearray(shape.getSideLengths());
+
+            for (Shape test : shapes) {
+                ArrayList<ArrayList<Double>> testVerts = test.getVertices();
+                ArrayList<Double> testLengths = toDoublearray(test.getSideLengths());
+
+                if (testVerts.contains(vertex)) {
+                    sideLengths.retainAll(testLengths);
+                    if (sideLengths.size() > 0) {
+                        result.add(test);
+                    }
+                }
+
+
+            }
+        }
+
+        return result;
+    }
+
 
 }
